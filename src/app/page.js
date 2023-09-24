882ebea952cb968158efe76c7fafa1bc9866c5dc
Alex Tenczar <24301038+alextenczar@users.noreply.github.com>
2023-09-24 17:02:55 -0400

    Cleaned up files

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
 import styles from './page.module.css'
-
 import dynamic from 'next/dynamic';
-
 const Clock = dynamic(() => import('./components/Clock'), { ssr: false });
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Alex Tenczar</h1>
-      <div>
+
+      <div id="about">
         <p>
           Hi, I&apos;m Alex &ndash; a Full Stack Developer. I graduated with my Master in Science in Information technology from the <a target="_blank" rel="noopener noreferrer" href="https://www.unh.edu/">University of New Hampshire</a>. In my free time I often find myself building web applications around modern SEO, accessibility, and security practices. When I&apos;m not building something, I enjoy travelling and taking photos. I&apos;m currently working as a contract developer for various Universities Across the United States.
         </p>
@@ -16,7 +15,7 @@ export default function Home() {
       <a target="_blank" rel="noopener noreferrer" href="https://www.notion.so/alextenczar/Resume-Alex-Tenczar-5d53c29076404671bcf568ec745feb03?pvs=4">Resume</a><br />
       <div>
       </div>
-      <div>
+      <div id="contact">
         <h2>Get In Touch:</h2>
         <a target="_blank" rel="noopener noreferrer" href="https://hcn2tnwe8l1.typeform.com/to/YlJlLq5w">EMAIL</a><br></br>
         <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alexander-tenczar">Linkedin</a><br />
