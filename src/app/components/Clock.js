acb1c147ccb8e3a6239532dae7af4407f48b2979
Alex Tenczar <24301038+alextenczar@users.noreply.github.com>
2023-09-30 14:02:24 -0400

    Added UTC and various changes to site

--- src/app/components/Clock.js
+++ src/app/components/Clock.js
@@ -1,11 +1,11 @@
-"use client"
+'use client'
 
 import { useState, useEffect } from 'react';
 import { DateTime } from 'luxon';
-import styles from '../page.module.css'
 
 const Clock = ({ timezone }) => {
     const [currentTime, setCurrentTime] = useState(DateTime.local().setZone(timezone));
+    const utcOffset = currentTime.offset / 60; // Get the UTC offset in hours
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -17,12 +17,17 @@ const Clock = ({ timezone }) => {
 
     const formattedTime = currentTime.toFormat('hh:mm:ss a');
 
+    // Format UTC offset in (UTC +/-HH:MM) format
+    const formattedUtcOffset = utcOffset > 0 ? `+${String(utcOffset).padStart(2, '0')}` : String(utcOffset).padStart(2, '0');
+    const formattedUtcString = `(UTC ${formattedUtcOffset}:00)`;
+
     return (
         <div>
             <time dateTime={formattedTime}>
                 {formattedTime}
             </time>
-        </div >
+            <> {formattedUtcString}</>
+        </div>
     );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -2,28 +2,36 @@ import styles from './page.module.css'
 import dynamic from 'next/dynamic';
 const Clock = dynamic(() => import('./components/Clock'), { ssr: false });
 
+export const metadata = {
+  metadataBase: new URL('https://alextenczar.com'),
+  alternates: {
+    canonical: '/',
+  },
+}
+
 export default function Home() {
+  let local = 'New Hampshire, USA'
+  let encodedLocal = encodeURIComponent(local)
+
   return (
     <main className={styles.main}>
       <h1>Alex Tenczar</h1>
 
       <div id="about">
         <p>
-          Hi, I&apos;m Alex &ndash; a Full Stack Developer. I graduated with my Master in Science in Information technology from the <a target="_blank" rel="noopener noreferrer" href="https://www.unh.edu/">University of New Hampshire</a>. In my free time I often find myself building web applications around modern SEO, accessibility, and security practices. When I&apos;m not building something, I enjoy travelling and taking photos. I&apos;m currently working as a contract developer for various Universities Across the United States.
+          Hi, I&apos;m Alex &ndash; a Full Stack Developer. I graduated with my Master in Science in Information technology from the <a target="_blank" rel="noopener noreferrer" href="https://www.unh.edu/">University of New Hampshire</a>. In my free time I often find myself building web applications around modern SEO, accessibility, and security practices. When I&apos;m not building something, I enjoy travelling and <a target="_blank" rel="noopener noreferrer" href="https://vsco.co/alextenczar/gallery">taking photos</a>. I&apos;m currently working as a contract developer for various Universities Across the United States.
         </p>
       </div>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.notion.so/alextenczar/Resume-Alex-Tenczar-5d53c29076404671bcf568ec745feb03?pvs=4">Resume</a><br />
+      <a target="_blank" rel="noopener noreferrer" href="https://www.notion.so/alextenczar/Resume-Alex-Tenczar-5d53c29076404671bcf568ec745feb03?pvs=4">My Resume</a><br />
       <div>
       </div>
       <div id="contact">
-        <h2>Get In Touch:</h2>
-        <a target="_blank" rel="noopener noreferrer" href="https://hcn2tnwe8l1.typeform.com/to/YlJlLq5w">EMAIL</a><br></br>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alexander-tenczar">Linkedin</a><br />
-        <a target="_blank" rel="noopener noreferrer" href="https://github.com/alextenczar">Github</a><br />
-        <a target="_blank" rel="noopener noreferrer" href="https://vsco.co/alextenczar/gallery">Photography</a><br />
+        <h2>Connect with me on</h2>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alexander-tenczar">Linkedin</a>, <a target="_blank" rel="noopener noreferrer" href="https://github.com/alextenczar">Github</a>, or send me an <a target="_blank" rel="noopener noreferrer" href="https://hcn2tnwe8l1.typeform.com/to/YlJlLq5w">EMAIL</a>
       </div>
 
-      <span><span className={styles.locationDot}></span><p style={{ display: 'inline-block' }}>New Hampshire, USA</p><Clock timezone="America/New_York" /></span>
-    </main>
+      <span><a target="_blank" rel="noopener noreferrer" className='local' href={`https://www.google.com/maps/search/${encodedLocal}`} style={{ display: 'inline-block' }}>{local} </a></span>
+      <br></br><Clock timezone="America/New_York" />
+    </main >
   )
 }
