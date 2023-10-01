92dd34ae2f4e72413ea5a7780c9d8856e5d7d6f5
Alex Tenczar <24301038+alextenczar@users.noreply.github.com>
2023-10-01 14:57:18 -0400

    Added security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
+const securityHeaders = [{
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+},
+{
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(self)'
+},
+{
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+},
+{
+    key: 'Referrer-Policy',
+    value: 'strict-origin'
+},
+]
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: securityHeaders,
+            }
+        ]
+    },
+}
 
 module.exports = nextConfig
