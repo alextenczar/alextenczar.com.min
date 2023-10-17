b2338c16d315f9abc26835c120aa9fbade5cf1d2
Alex Tenczar <24301038+alextenczar@users.noreply.github.com>
2023-10-16 21:54:38 -0400

    Added Favicon References

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,8 +11,13 @@ export const metadata = {
   themeColor: 'white',
   manifest: '/site.webmanifest',
   icons: {
-    icon: [{ url: "/favicon-16x16.png", sizes: "16x16", type: 'image/png' },
-    { url: "/favicon-32x32.png", sizes: "32x32", type: 'image/png' }],
+    icon: [
+      { url: "/android-chrome-512x512.png", sizes: "512x512", type: 'image/png' },
+      { url: "/android-chrome-192x192.png", sizes: "192x192", type: 'image/png' },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: 'image/png' },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: 'image/png' },
+      { url: "/favicon.ico", sizes: "any", type: 'image/x-icon' }
+    ],
     apple: '/apple-touch-icon.png'
   }
 }
